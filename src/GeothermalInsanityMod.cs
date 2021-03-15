0d8be3a287bab2b4988a2f40011bb06a23673477
botenredwolf <80605251+botenredwolf@users.noreply.github.com>
2021-03-15 16:28:53 -0500

    Revert "Revert "Update GeothermalInsanityMod.cs""
    This reverts commit 011196c668d475949fa58ce15b591f58fe440363.

--- src/GeothermalInsanityMod.cs
+++ src/GeothermalInsanityMod.cs
@@ -12,9 +12,30 @@ using Vintagestory.API.MathTools;
 namespace GeothermalInsanity {
   public class GeothermalInsanityMod : ModSystem {
     public override void Start(ICoreAPI api) {
-      api.Event.OnGetClimate += (ref ClimateCondition climate, BlockPos pos, EnumGetClimateMode mode, double totalDays) => {
-        climate.Temperature += api.World.SeaLevel - pos.Y;
-      };
+		//some witchcraft from goxmeor to start this all
+    	api.Event.OnGetClimate += (ref ClimateCondition climate, BlockPos pos, EnumGetClimateMode mode, double totalDays) => {
+			//define some shit we need for intermediary
+			float subDepth = 0, seaLevelDifference = 0, blockAbove = 0;
+			bool isUnderground = true;
+			var rainMapHeight = api.World.BlockAccessor.GetRainMapHeightAt(pos);
+			//checks if you're below sea level, assigns level difference
+			if (pos.Y < api.World.SeaLevel) {
+				seaLevelDifference = (api.World.SeaLevel - pos.Y);
+			}
+			//attempts to figure out if there's an impermeable block somewhere above your head (are you underground?)
+			if (pos.Y < rainMapHeight){
+				isUnderground = true;
+				blockAbove = (rainMapHeight - pos.Y);
+			}
+			//grabs the lower value of either depth below sea level or depth below surface as value for subDepth
+			if (isUnderground == true && seaLevelDifference > 0){
+				subDepth = Math.Min(seaLevelDifference, blockAbove);
+			}
+			//doing the actual temperature shifts
+			if (subDepth > 0) {
+				climate.Temperature = 5 + (10 / subDepth);
+			}
+   		};
+      }
     }
   }
-}
